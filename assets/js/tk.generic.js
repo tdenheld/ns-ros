5c2a21593b17d8d35aaaf4c1d5b3a2be845a2719
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2018-11-02 16:54:59 +0100

    1st page

--- assets/js/tk.generic.js
+++ assets/js/tk.generic.js
@@ -1,93 +1,118 @@
 // generic javascript
 // ------------------------------------------------------------
-$(document).ready(function(){
+$(document).ready(function () {
 
-// global vars
-// ------------------------------------------------------------
-// ------------------------------------------------------------
-var window_width = Number;
-var window_height = Number;
-var mobile = Boolean;
-var scrolled = false;
-
-
-	
-// update screen height
-// ------------------------------------------------------------	
-function update_window_size() {
-	window_width = $(window).innerWidth();
-	window_height = $(window).innerHeight();
-	
-	// set breakpoints
-	if (window_width > 912) {
-		mobile = false;
-	} else {
-		mobile = true;
-	};
-};
-update_window_size();
-	
-// update when resizing
-$(window).resize(function() {
-	update_window_size();
-});
+    // global vars
+    // ------------------------------------------------------------
+    // ------------------------------------------------------------
+    var window_width = Number;
+    var window_height = Number;
+    var mobile = Boolean;
+    var scrolled = false;
 
 
 
+    // update screen height
+    // ------------------------------------------------------------	
+    function updateWindowSize() {
+        window_width = $(window).innerWidth();
+        window_height = $(window).innerHeight();
 
-// functionality that"s on linked on scroll
-// ------------------------------------------------------------
-
-$(window).scroll(function() {
-	scrolled = true;
-	if (scrolled) {
-		//requestAnimationFrame(scrolling);
-	};
-});
-
-function scrolling() {
-	var pos = $(window).scrollTop();
-	// fade arrow scroll down button
-	scrolled = false;
-};
-
-
-
-
-
-// include html
-// ------------------------------------------------------------
-function include_html() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("include");
-        if (file) {
-        	/*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {
-                        elmnt.innerHTML = this.responseText;
-                    }
-                    if (this.status == 404) {
-                        elmnt.innerHTML = "Page not found.";
-                    }
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("include");
-                    include_html();
-                };
-            };
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
+        // set breakpoints
+        if (window_width > 912) {
+            mobile = false;
+        } else {
+            mobile = true;
         };
     };
-};
-include_html();
+    updateWindowSize();
+
+    // update when resizing
+    $(window).resize(function () {
+        updateWindowSize();
+    });
+
+
+
+
+    // functionality that"s on linked on scroll
+    // ------------------------------------------------------------
+
+    $(window).scroll(() => {
+        scrolled = true;
+        if (scrolled) {
+            //requestAnimationFrame(scrolling);
+        };
+    });
+
+    function scrolling() {
+        var pos = $(window).scrollTop();
+        // fade arrow scroll down button
+        scrolled = false;
+    };
+
+
+
+
+    // sticky bar
+    // ------------------------------------------------------------
+    function stickyBar() {
+        var controller = new ScrollMagic.Controller();
+        var id = document.getElementById("js-sticky-trig");
+        if (id) {
+            $(".js-sticky-bar").addClass("is-active");
+            var scrll = new ScrollMagic.Scene({
+                    triggerElement: "#js-sticky-trig",
+                    duration: 999999,
+                })
+                .triggerHook(0.9)
+                .offset(0)
+                .on("start", function () {
+                    $(".js-sticky-bar").toggleClass("is-active");
+                })
+                //.addIndicators()
+                .addTo(controller);
+        };
+    };
+    stickyBar();
+
+
+
+
+
+    // include html
+    // ------------------------------------------------------------
+    function includeHtml() {
+        var z, i, elmnt, file, xhttp;
+        /*loop through a collection of all HTML elements:*/
+        z = document.getElementsByTagName("*");
+        for (i = 0; i < z.length; i++) {
+            elmnt = z[i];
+            /*search for elements with a certain atrribute:*/
+            file = elmnt.getAttribute("include");
+            if (file) {
+                /*make an HTTP request using the attribute value as the file name:*/
+                xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState == 4) {
+                        if (this.status == 200) {
+                            elmnt.innerHTML = this.responseText;
+                        }
+                        if (this.status == 404) {
+                            elmnt.innerHTML = "Page not found.";
+                        }
+                        /*remove the attribute, and call this function once more:*/
+                        elmnt.removeAttribute("include");
+                        includeHtml();
+                    };
+                };
+                xhttp.open("GET", file, true);
+                xhttp.send();
+                /*exit the function:*/
+                return;
+            };
+        };
+    };
+    includeHtml();
 
 });
