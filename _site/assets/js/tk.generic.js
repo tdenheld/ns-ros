278919124c2a09f3640151781ce4df5d1dbb8162
Tibor den Held <tibordenheld@Tibors-MacBook-Pro.local>
2018-12-11 07:47:32 +0100

    fix tk-choice-selector margin | tab key in birthday field

--- _site/assets/js/tk.generic.js
+++ _site/assets/js/tk.generic.js
@@ -207,7 +207,7 @@ $(document).ready(function () {
 
                 // only numbers are valid input
                 input.keydown(function (e) {
-                    if (!((e.which >= 48 && e.which <= 57) || (e.which >= 96 && e.which <= 105) || (e.which === 8))) {
+                    if (!((e.which >= 48 && e.which <= 57) || (e.which >= 96 && e.which <= 105) || (e.which === 8) || (e.which === 9))) {
                         return false;
                     };
                 });
